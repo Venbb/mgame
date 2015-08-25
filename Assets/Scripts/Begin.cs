21b074469f61d59afdd5cb69905d1f4664a034a0
wenbiao <wenbiao@enveesoft.com>
2015-08-25 18:25:32 +0800

    no message

--- Assets/Scripts/Begin.cs
+++ Assets/Scripts/Begin.cs
@@ -13,18 +13,28 @@ public class Begin : MonoBehaviour
 //		mgr.Start ();
 //		mgr.DoString ("print('helo lua')");
 
-		AndroidJavaClass ac = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject ao = ac.GetStatic<AndroidJavaObject> ("currentActivity");
-		Debug.Log ("ao==============1:" + ao);
-		ao.Call ("SetListener", name, "onBillingFinish");
-		Debug.Log ("ao==============2:" + ao);
-
-		ao.Call ("Order", "", 1);
+//		AndroidJavaClass ac = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+//		AndroidJavaObject ao = ac.GetStatic<AndroidJavaObject> ("currentActivity");
+//		Debug.Log ("ao==============1:" + ao);
+//		ao.Call ("SetListener", name, "onBillingFinish");
+//		Debug.Log ("ao==============2:" + ao);
+//
+//		ao.Call ("Order", "", 1);
+		AndroidJavaClass ac = new AndroidJavaClass ("com.iap.mm.IAPController");
+		ac.CallStatic ("init", "300009184004", "2F3CE83A8367CC82CF5F15D1A9269CEA", name, "onBillingFinish");
 	}
 
 	public void hhs ()
 	{
 		Debug.Log ("klshdadhakjhk=====");
+	}
+	void OnGUI ()
+	{
+		if (GUI.Button (new Rect (30, 30, 200, 50), "立即购买"))
+		{
+			AndroidJavaClass ac = new AndroidJavaClass ("com.iap.mm.IAPController");
+			ac.CallStatic ("order", "30000918400401", 1);
+		}
 	}
 	// Update is called once per frame
 	void Update ()
